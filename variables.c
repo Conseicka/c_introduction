5eeaa7b975fa98f54a4ce0247a5e24efab5e1c3d
Erick Saavedra <>
2021-12-21 14:16:12 -0600

    declaracion de variables

---
+++ variables.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+//Variable dleclaration
+int a, b, c;
+
+int main()
+{
+//Variable initialization
+    a = 1;
+    b = 34;
+
+    c = a + b;
+    printf("Value of sum is: %d", c);
+    
+    return 0;
+}
